b08e3141579e623615890552c13a65fa11652840
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-11-17 15:34:04 -0700

    add test functionality to todo module

--- src/todo.js
+++ src/todo.js
@@ -1,19 +1,49 @@
 function Todo(title = '', description = '', dueDate = null, priority = 3){
-	const _title = title;
-	const _description = description;
-	const _dueDate = dueDate;
-	const _priority = priority;
+	let _title = title;
+	let _description = description;
+	let _dueDate = dueDate;
+	let _priority = priority;
 
 	const getTitle = () => _title;
+	const setTitle = (newTitle) => {
+		if (typeof newTitle !== "string"){
+			throw TypeError('title must be a string');
+		}
+		return _title = newTitle; 
+	}	
 	const getDescription = () => _description;
+	const setDescription = (newDesc) => {
+		if (typeof newDesc !== "string"){
+			throw TypeError('description must be a string');
+		}
+		return _description = newDesc;
+	}
 	const getDueDate = () => _dueDate;
+	const setDueDate = (newDate) => {
+		if (newDate instanceof Date === false){
+			throw TypeError('due date must be a date object');
+		}
+		return _dueDate = newDate;
+	}
 	const getPriority = () => _priority;
+	const setPriority = (newPri) => {
+		if (typeof newPri !== "number"){
+			throw TypeError('priority must be a number');
+		}
+		return _priority = newPri;
+	}  
 
-	
-
-	return {getTitle, getDescription, getDueDate, getPriority};
+	return {
+		getTitle,
+		setTitle, 
+		getDescription,
+		setDescription, 
+		getDueDate,
+		setDueDate, 
+		getPriority,
+		setPriority
+	};
 }
 
 
-// module.exports.Todo = Todo;
 export default Todo
--- test/todo.test.js
+++ test/todo.test.js
@@ -1,10 +1,12 @@
 import Todo from '../src/todo';
+import {format} from 'date-fns';
  
 describe('testing functionality of todo factory function', () => {
 	let todoTest;
 	beforeEach(function(){
 		todoTest = Todo();
 	})
+
 	it('should instantiate a default todo object (test getters)', () => {
 		expect(todoTest.getTitle()).toBe('');
 		expect(todoTest.getDescription()).toBe('');
@@ -12,10 +14,43 @@ describe('testing functionality of todo factory function', () => {
 		expect(todoTest.getPriority()).toBe(3);
 	})
 	
-	it('should set property (title)', () => {
+	it('should set title', () => {
 		todoTest.setTitle('testTitle');
 		expect(todoTest.getTitle()).toBe('testTitle');
-	} )
+	})
 
-	//test types at some point
+	it('should throw type error when non-string title is set', () => {
+		expect(() => todoTest.setTitle(4)).toThrow();
+		expect(() => todoTest.setTitle('test')).not.toThrow();
+	})
+	
+	it('should set description', () => {
+		todoTest.setDescription('test description');
+		expect(todoTest.getDescription()).toBe('test description');
+	})
+
+	it('should throw type error when non-string description is set', () => {
+		expect(() => todoTest.setDescription(4)).toThrow();
+		expect(() => todoTest.setDescription('test')).not.toThrow();
+	})
+
+	it('should get default dueDate equal to null', () => {
+		expect(todoTest.getDueDate()).toBe(null);
+	})
+
+	it('setDue date should throw error when non-date obj is used', () => {
+		let today = new Date();
+		expect(() => todoTest.setDueDate(today)).not.toThrow();
+		expect(() => todoTest.setDueDate(5)).toThrow();
+	})
+
+	it('getPriority should get the default of 3', () => {
+		expect(todoTest.getPriority()).toBe(3);
+	})
+
+	it('setPriority should throw error when non number is used', () => {
+		expect(() => todoTest.setPriority('green')).toThrow();
+		expect(() => todoTest.setPriority(1)).not.toThrow();
+	})
+
 })
