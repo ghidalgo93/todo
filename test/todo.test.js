e2aae41b5ea53a7bf4348bcafc4e1ed0f640d7cf
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-11-17 14:16:15 -0700

    add get/set test for todo obj

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,11 +1,21 @@
 import Todo from '../src/todo';
  
 describe('testing functionality of todo factory function', () => {
-	it('should instantiate a default todo object', () => {
-		const todoTest = Todo();
+	let todoTest;
+	beforeEach(function(){
+		todoTest = Todo();
+	})
+	it('should instantiate a default todo object (test getters)', () => {
 		expect(todoTest.getTitle()).toBe('');
 		expect(todoTest.getDescription()).toBe('');
 		expect(todoTest.getDueDate()).toBe(null);
 		expect(todoTest.getPriority()).toBe(3);
 	})
+	
+	it('should set property (title)', () => {
+		todoTest.setTitle('testTitle');
+		expect(todoTest.getTitle()).toBe('testTitle');
+	} )
+
+	//test types at some point
 })
