836e717c0be61c42f697f521954bb959eb67d04d
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-11-18 16:50:22 -0700

    add: webpack devserver. start: dom manipulation

--- src/domController.js
+++ src/domController.js
@@ -1,4 +1,13 @@
 const domController = (() => {
+	let _todoInput = document.getElementById('todoItem');
+	let _addBtn = document.getElementById('add-btn');
+
+
+	const handleAdd = () => {
+		let newInput = _todoInput.value;
+		_todoInput.value = '' 
+	}
+	_addBtn.addEventListener('click', handleAdd, false);
 
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import Todo from './todo';
+import Project from './project';
+import Container from './container';
+
+//app init
+const defaultProj = Project();
+Container.addProject(defaultProj);
+
+//dom init  
+const todoInput = document.getElementById('todo-item');
+const addBtn = document.getElementById('add-btn');
+const projPulldown = document.getElementById('project-pulldown');
 
 
-let test = Todo();
+//event handlers
+const handleAddClick = () => {
+	let newInput = todoInput.value;
+	todoInput.value = '' 
+}
+addBtn.addEventListener('click', handleAddClick, false);
+
+
+
 
