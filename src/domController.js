6cc0ed483d0155457b885ef5567adc4f162a5c0d
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-11-19 10:09:59 -0700

    add html init and domController

--- src/domController.js
+++ src/domController.js
@@ -1,14 +1,26 @@
 const domController = (() => {
-	let _todoInput = document.getElementById('todoItem');
-	let _addBtn = document.getElementById('add-btn');
+	const _headline = document.getElementById('headline');
+	const _newTodoField = document.getElementById('new-todo-field');
+	// const _todoLbl = document.getElementById('todo-label');
+	const _todoInp = document.getElementById('todo-item');
+	// const _projectLbl = document.getElementById('project-label');
+	const _projectPullDwn = document.getElementById('project-pulldown'); 
+	const _projectDefault = document.getElementById('project-default');
+	const _addBtn = document.getElementById('add-btn');
+	const _todoContainer = document.getElementById('todo-container');
 
 
 	const handleAdd = () => {
-		let newInput = _todoInput.value;
-		_todoInput.value = '' 
+		let newInput = _todoInp.value;
+		console.log(newInput);
+		_todoInp.value = '' 
 	}
+
+	//event listeners
 	_addBtn.addEventListener('click', handleAdd, false);
 
+	return {
+	}
 })();
 
 export default domController
--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import Todo from './todo';
 import Project from './project';
 import Container from './container';
+import domController from './domController';
 
 //app init
 const defaultProj = Project();
 Container.addProject(defaultProj);
 
-//dom init  
-const todoInput = document.getElementById('todo-item');
-const addBtn = document.getElementById('add-btn');
-const projPulldown = document.getElementById('project-pulldown');
 
 
-//event handlers
-const handleAddClick = () => {
-	let newInput = todoInput.value;
-	todoInput.value = '' 
-}
-addBtn.addEventListener('click', handleAddClick, false);
+
 
 
 
