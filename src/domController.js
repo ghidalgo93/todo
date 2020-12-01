adc6512055ff3b54c8d9a4b96eba7e964bb076a7
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-12-01 14:19:26 -0700

    add eslint and prettier via ALE

--- src/domController.js
+++ src/domController.js
@@ -1,101 +1,102 @@
-import {removeAllChildNodes} from './helpers';
+import { removeAllChildNodes } from "./helpers";
 
 const domController = (() => {
-	//dom elements
-	const _todoContainer = document.getElementById('todo-container');
+  // dom elements
+  const _todoContainer = document.getElementById("todo-container");
 
-	const _todoInp = document.getElementById('todo-item');
-	const getTodoTitle = () => _todoInp.value;
-	const clearTodoTitle = () => _todoInp.value = '';
+  const _todoInp = document.getElementById("todo-item");
+  const getTodoTitle = () => _todoInp.value;
+  const clearTodoTitle = () => {
+    _todoInp.value = "";
+    return _todoInp;
+  };
 
-	const _todoDuedate = document.getElementById('todo-duedate');
-	const getDuedate = () => _todoDuedate.value;
+  const _todoDuedate = document.getElementById("todo-duedate");
+  const getDuedate = () => _todoDuedate.value;
 
-	const _projectPullDwn = document.getElementById('project-pulldown'); 
-	const getProjectTitle = () => _projectPullDwn.value;
+  const _projectPullDwn = document.getElementById("project-pulldown");
+  const getProjectTitle = () => _projectPullDwn.value;
 
-	const _addBtn = document.getElementById('add-btn');
-	const getAddTodoBtn = () => _addBtn;
+  const _addBtn = document.getElementById("add-btn");
+  const getAddTodoBtn = () => _addBtn;
 
-	const _addProjectBtn = document.getElementById('add-project');
-	const getAddProjectBtn = () => _addProjectBtn;
+  const _addProjectBtn = document.getElementById("add-project");
+  const getAddProjectBtn = () => _addProjectBtn;
 
-	//dom manipulation functions
-	const updateDom = (container) => {
-		resetInputs(container);
-		removeAllChildNodes(_todoContainer);
-		renderTodosTable(container);
-	}
+  // dom manipulation functions
 
-	const resetInputs = (container) => {
-		clearTodoTitle();
-		removeAllChildNodes(_projectPullDwn);
-		renderProjectsPulldown(container);
-	}	
+  const renderProjectsPulldown = (container) => {
+    const projects = container.getProjects();
 
-	const renderProjectsPulldown = (container) => {
-		let projects = container.getProjects();
+    for (const project of projects) {
+      const option = document.createElement("option");
+      option.value = project.getTitle();
+      option.innerHTML = project.getTitle();
+      _projectPullDwn.appendChild(option);
+    }
+  };
 
-		for (let project of projects){
-			let option = document.createElement('option');
-			option.value = project.getTitle(); 
-			option.innerHTML = project.getTitle();
-			_projectPullDwn.appendChild(option);
-		}
-	}
+  const resetInputs = (container) => {
+    clearTodoTitle();
+    removeAllChildNodes(_projectPullDwn);
+    renderProjectsPulldown(container);
+  };
 
+  const renderTodosTable = (container) => {
+    const projects = container.getProjects();
 
-	const renderTodosTable = (container) => {
-		let projects = container.getProjects();
+    for (const project of projects) {
+      const table = document.createElement("table");
+      const thead = document.createElement("thead");
+      const tbody = document.createElement("tbody");
+      const projectRow = document.createElement("tr");
+      const projectTitle = document.createElement("th");
+      projectTitle.innerHTML = project.getTitle();
 
-		for (let project of projects){
-			let table = document.createElement('table');
-			let thead = document.createElement('thead');
-			let tbody = document.createElement('tbody');
-			let projectRow = document.createElement('tr');
-			let projectTitle = document.createElement('th');
-			projectTitle.innerHTML = project.getTitle();
+      for (const todo of project.getTodos()) {
+        const todoRow = document.createElement("tr");
+        todoRow.dataset.title = todo.getTitle();
+        const todoCheck = document.createElement("input");
+        todoCheck.setAttribute("type", "checkbox");
+        const todoTitle = document.createElement("td");
+        todoTitle.innerHTML = todo.getTitle();
+        const removeTodo = document.createElement("button");
+        removeTodo.dataset.title = todo.getTitle();
+        removeTodo.classList.add("remove-btn");
+        removeTodo.innerHTML = "x";
+        todoRow.appendChild(todoCheck);
+        todoRow.appendChild(todoTitle);
+        if (todo.getDueDate()) {
+          const todoDuedate = document.createElement("td");
+          todoDuedate.innerHTML = `due: ${todo.getDueDate()}`;
+          todoRow.appendChild(todoDuedate);
+        }
+        todoRow.appendChild(removeTodo);
+        tbody.appendChild(todoRow);
+      }
 
-			for (let todo of project.getTodos()){
-				let todoRow = document.createElement('tr');
-				todoRow.dataset.title = todo.getTitle();
-				let todoCheck = document.createElement('input');
-				todoCheck.setAttribute('type', 'checkbox');
-				let todoTitle = document.createElement('td');
-				todoTitle.innerHTML = todo.getTitle();
-				let removeTodo = document.createElement('button');
-				removeTodo.dataset.title = todo.getTitle();
-				removeTodo.classList.add('remove-btn');
-				removeTodo.innerHTML = 'x';
-				todoRow.appendChild(todoCheck);
-				todoRow.appendChild(todoTitle);
-				if (todo.getDueDate()){
-					let todoDuedate = document.createElement('td');
-					todoDuedate.innerHTML = `due: ${todo.getDueDate()}`;
-					todoRow.appendChild(todoDuedate);
-				}
-				todoRow.appendChild(removeTodo);
-				tbody.appendChild(todoRow);
-			}
+      projectRow.appendChild(projectTitle);
+      thead.appendChild(projectRow);
+      table.appendChild(thead);
+      table.appendChild(tbody);
+      _todoContainer.appendChild(table);
+    }
+  };
 
-			projectRow.appendChild(projectTitle);
-			thead.appendChild(projectRow);
-			table.appendChild(thead);
-			table.appendChild(tbody);
-			_todoContainer.appendChild(table);
-		}
-	}
-	
+  const updateDom = (container) => {
+    resetInputs(container);
+    removeAllChildNodes(_todoContainer);
+    renderTodosTable(container);
+  };
 
-	return {
-		getAddTodoBtn,
-		getAddProjectBtn, 
-		getTodoTitle, 
-		getDuedate,
-		getProjectTitle,
-		updateDom
-	}
+  return {
+    getAddTodoBtn,
+    getAddProjectBtn,
+    getTodoTitle,
+    getDuedate,
+    getProjectTitle,
+    updateDom,
+  };
 })();
 
-export default domController
-
+export default domController;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import Todo from './todo';
-import Project from './project';
-import Container from './container';
-import domController from './domController';
-import addTodo from './helpers';
+import Todo from "./todo";
+import Project from "./project";
+import Container from "./container";
+import domController from "./domController";
 
-//app init
-const defaultProject = Project('main');
+// app init
+const defaultProject = Project("main");
 Container.addProject(defaultProject);
 domController.updateDom(Container);
 
 // event listeners
-domController.getAddTodoBtn().addEventListener('click', () => {
-	let project = Container.getProjectByTitle(domController.getProjectTitle());
-	let todo = Todo(domController.getTodoTitle());
-	todo.setDueDate(domController.getDuedate());
-	project.addTodo(todo);
-	domController.updateDom(Container);
+domController.getAddTodoBtn().addEventListener("click", () => {
+  const project = Container.getProjectByTitle(domController.getProjectTitle());
+  const todo = Todo(domController.getTodoTitle());
+  todo.setDueDate(domController.getDuedate());
+  project.addTodo(todo);
+  domController.updateDom(Container);
 });
 
-domController.getAddProjectBtn().addEventListener('click', () => {
-	let projectName = Project(prompt('New project name: '))
-	Container.addProject(projectName);
-	domController.updateDom(Container);
-})
-
-
+domController.getAddProjectBtn().addEventListener("click", () => {
+  const projectName = Project(prompt("New project name: "));
+  Container.addProject(projectName);
+  domController.updateDom(Container);
+});
