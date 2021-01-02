2efe45180d3f8536aa29d845a216a54ff38bd5de
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-01-01 18:26:46 -0700

    reformating project

--- test/domController.test.js
+++
@@ -1,7 +0,0 @@
-import domController from "../src/domController";
-
-describe("tests functionality of dom manupulation", () => {
-  it("createProjectElem should return a project element", () => {
-    expect(validateProjectName("testProjectName")).toBe("testProjectName");
-  });
-});
